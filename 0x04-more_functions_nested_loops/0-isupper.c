cdbb6600f0da25cae8855ab29e571f7edc95b09a
Opeade09 <de_exploiters@hotmail.com>
2023-07-21 13:01:49 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters.
+ * @c:char to check
+ * Return: 0 for anything else or 1 for uppercase character
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
